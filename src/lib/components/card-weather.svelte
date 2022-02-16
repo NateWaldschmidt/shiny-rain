841a1cb30a6fd8aadaa9f529fc79fd06eafa20e1
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-02-16 11:10:44 -0600

    Added styles and functionality to change the location.

--- src/lib/components/card-weather.svelte
+++ src/lib/components/card-weather.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
     import getCoordinates      from '$lib/utilities/get-coordinates';
     import getWeatherAPIStatus from '$lib/utilities/get-weather-api-status';
+    import getWeekDayString    from '$lib/utilities/get-day-string';
 
     /** The title of this weather card. */
     export const cardTitle = 'Weather this Week';
 
     /** The city we are searching for weather data from. */
-    export const city = 'Geneva';
+    export let city = 'Geneva';
 
     /** The state we are searching for weather data from. */
-    export const state = 'Illinois'
+    export let state = 'Illinois';
 
     /**
      * Does the API request for the weather data using
@@ -19,7 +20,10 @@
      * @author  Nathaniel Waldschmidt <Nathaniel.Waldsch@gmail.com>
      * @returns { object } The weather data from the API.
      */
-    let weekWeatherData = async function (): Promise<any> {
+    export async function weekWeatherData(): Promise<any> {
+        // Sets to undefined to show the loading part.
+        weatherData = undefined;
+
         // Ensures everything is gucci.
         await getWeatherAPIStatus();
 
@@ -38,37 +42,99 @@
         if (weatherWeeklyRes.status !== 200) {
             throw new Error(`(${weatherWeeklyRes.status}) Weather API returned an error.`);
         }
-        const weatherWeeklyData = await weatherWeeklyRes.json();
-
-        console.log(weatherWeeklyData);
-
-        return weatherWeeklyData;
+        weatherData = await weatherWeeklyRes.json();
     }
+
+    let weatherData: any;
 </script>
 
-<article>
-    <ul>
-        {#await weekWeatherData()}
-            <li>Loading weather data...</li>
-        {:then weatherData}
+<article id="card">
+    <h2 id="card-title">7-Day Forecast</h2>
+        {#if weatherData == undefined}
+            {#await weekWeatherData()}
+                <p>Loading data for {city}, {state}...</p>
+            {/await}
+        {:else}
             {#each weatherData.properties.periods as day}
-                <li>
-                    <p class="sr-datetime">
-                        <time datetime="{day.startTime}">
-                            {day.name}
-                        </time>
-                    </p>
-                    <p class="sr-conditions" data-day-time="{day.isDaytime}">
-                        {day.shortForecast}
-                    </p>
-                    <p class="sr-low-temp">
-                        {day.temperature} &#176;{day.temperatureUnit}
-                    </p>
-                    <!-- <p class="sr-high-temp">{day.temperature} &#176;F</p> -->
-                </li>
+                {#if day.isDaytime}
+                    <ul id="forecast-list">
+                        <li>
+                            <p class="sr-datetime">
+                                <time datetime="{day.startTime}">
+                                    {getWeekDayString(day.startTime)}
+                                </time>
+                            </p>
+                            <p class="sr-conditions" data-day-time="{day.isDaytime}">
+                                {day.shortForecast}
+                            </p>
+                            <p class="sr-low-temp">
+                                {day.temperature} &#176;{day.temperatureUnit}
+                            </p>
+                            <!-- <p class="sr-high-temp">{day.temperature} &#176;F</p> -->
+                        </li>
+                    </ul>
+                {/if}
             {/each}
-        {:catch err}
-                <li>({err}) Error loading weather data...</li>
-        {/await}
-    </ul>
+        {/if}
 </article>
+
+<style lang="scss">
+    #card {
+        max-width: 810px;
+        margin: 0 auto;
+        padding: 0.5rem;
+        border-radius: 3px;
+        box-sizing: border-box;
+        
+        background-color: var(--color-bg-card);
+        backdrop-filter: saturate(1500%);
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+
+        color: var(--color-font-card);
+    }
+
+    #card-title {
+        margin: 0;
+        padding-bottom: 0.4rem;
+        border-bottom: solid 1px rgba(255,255,255,0.28);
+
+        font-size: 0.85rem;
+        font-weight: var(--font-weight-reg);
+    }
+
+    #forecast-list {
+        list-style: none;
+
+        margin: 0;
+        padding: 0;
+
+        & > li {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            padding: 0.65rem;
+
+            // Avoids a border on the last element.
+            &:not(:last-of-type) {
+                border-bottom: solid 1px rgba(255,255,255,0.28);
+            }
+
+            font-size: 1rem;
+
+            & > * {
+                margin: 0;
+            }
+        }
+    }
+
+    .sr-datetime {
+        width: 7rem;
+    }
+    .sr-low-temp {
+        width: 7rem;
+
+        text-align: right;
+        font-weight: var(--font-weight-bold);
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
     import WeatherCard from '../lib/components/card-weather.svelte';
+
+    let weatherCard: WeatherCard;
+
+    let city: string;
+    let state: string;
 </script>
 
 <main>
     <h1>Shiny Rain</h1>
-    <WeatherCard />
+    <form on:submit|preventDefault={async() => await weatherCard.weekWeatherData()}>
+        <label for="sr-city">City/ Town</label>
+        <input id="sr-city" type="text" bind:value={city} />
+        <label for="sr-state">State/ Province</label>
+        <input id="sr-state" type="text" bind:value={state} />
+        <button type="submit">Load Weather</button>
+    </form>
+    <WeatherCard bind:this={weatherCard} bind:city={city} bind:state={state} />
 </main>
+
+<style lang="scss">
+    h1 {
+        margin: 0;
+
+        font-size: 1.25rem;
+    }
+</style>
